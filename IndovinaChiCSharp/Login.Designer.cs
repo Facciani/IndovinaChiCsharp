87ad89c6371f74cd19c8fc873140377cbc0a5224
Facciani <alex03fa@gmail.com>
2021-12-17 09:55:52 +0100

    Pagina li login

---
+++ IndovinaChiCSharp/Login.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace IndovinaChiCSharp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.NomeUtente = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NomeUtente
+            // 
+            this.NomeUtente.Location = new System.Drawing.Point(158, 162);
+            this.NomeUtente.Name = "NomeUtente";
+            this.NomeUtente.Size = new System.Drawing.Size(250, 20);
+            this.NomeUtente.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(567, 345);
+            this.Controls.Add(this.NomeUtente);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.Text = "Benvenuto!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NomeUtente;
+    }
+}
+
--- IndovinaChiCSharp/Program.cs
+++ IndovinaChiCSharp/Program.cs
@@ -16,7 +16,7 @@ namespace IndovinaChiCSharp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
