c4b98d4260675d340d63b9774f524a9980536fc2
Facciani <alex03fa@gmail.com>
2021-12-28 17:19:28 +0100

    Creazione design e funzionalità pagina di login

--- IndovinaChiCSharp/Login.Designer.cs
+++ IndovinaChiCSharp/Login.Designer.cs
@@ -30,23 +30,69 @@ namespace IndovinaChiCSharp
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.NomeUtente = new System.Windows.Forms.TextBox();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbUser1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // NomeUtente
+            // label5
             // 
-            this.NomeUtente.Location = new System.Drawing.Point(158, 162);
-            this.NomeUtente.Name = "NomeUtente";
-            this.NomeUtente.Size = new System.Drawing.Size(250, 20);
-            this.NomeUtente.TabIndex = 0;
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(29, 1);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(298, 270);
+            this.label5.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("Berlin Sans FB Demi", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(90, 355);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tbUser1
+            // 
+            this.tbUser1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbUser1.Font = new System.Drawing.Font("Berlin Sans FB Demi", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUser1.Location = new System.Drawing.Point(67, 300);
+            this.tbUser1.Margin = new System.Windows.Forms.Padding(2);
+            this.tbUser1.Name = "tbUser1";
+            this.tbUser1.Size = new System.Drawing.Size(223, 32);
+            this.tbUser1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB Demi", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 254);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome giocatore:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.ClientSize = new System.Drawing.Size(567, 345);
-            this.Controls.Add(this.NomeUtente);
+            this.ClientSize = new System.Drawing.Size(357, 410);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbUser1);
+            this.Controls.Add(this.button1);
             this.ForeColor = System.Drawing.Color.Black;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Login";
@@ -58,7 +104,12 @@ namespace IndovinaChiCSharp
 
         #endregion
 
-        private System.Windows.Forms.TextBox NomeUtente;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbUser1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
