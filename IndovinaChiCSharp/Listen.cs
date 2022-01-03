6888e3633943875dc49af422889c2cad9ddf692f
Facciani <alex03fa@gmail.com>
2022-01-03 16:54:29 +0100

    Creazione classe Listen e prova di connessione

---
+++ IndovinaChiCSharp/Listen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndovinaChiCSharp
+{
+    class Listen
+    {
+        //Condivisa c;
+        //Gestore_Packet gPacket;
+        UdpClient client;
+
+        public Listen(UdpClient c)
+        {
+            client = c;
+            //c = Condivisa.getIstance();
+            //gPacket = Gestore_Packet.GetInstance();
+        }
+
+        public void Run()
+        {
+            while(true)
+            {
+                IPEndPoint riceveEP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] dataReceived = client.Receive(ref riceveEP);
+                String risposta = Encoding.ASCII.GetString(dataReceived);
+            }
+        }
+
+    }
+}
--- IndovinaChiCSharp/Login.cs
+++ IndovinaChiCSharp/Login.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,20 @@ namespace IndovinaChiCSharp
 {
     public partial class Login : Form
     {
+        Listen l;
+        UdpClient client = new UdpClient(666);
         public Login()
         {
+            l = new Listen(client);
             InitializeComponent();
+            Thread t = new Thread(l.Run);
+            t.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(tbUser1.Text);
+            client.Send(data, data.Length, "localhost", 666);
         }
     }
 }
