2e31825204f53790973aba4d2d00d5e17d3a5fdf
AndreaLietti <andrealietti11@gmail.com>
2022-01-03 16:58:07 +0100

    creazione gestore dei pacchetti e della classe condivisa

---
+++ IndovinaChiCSharp/Condivisa.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndovinaChiCSharp
+{
+    class Condivisa
+    {
+        UdpClient serverRicezione;
+        String nome;
+        String nomeDestinatario;
+        static Condivisa instance = null;
+        bool mittente;
+        UdpClient serverInvio;
+        Login frame;
+        String messaggio = "";
+        bool connected = false;
+
+        private Condivisa()
+        {
+            // gPacket = new Gestore_Packet();
+            this.serverRicezione = new UdpClient(12345);
+            serverInvio = new UdpClient();
+            mittente = false;
+            nomeDestinatario = "";
+            nome = "Pluto";
+        }
+
+
+
+
+
+        public void setFrame(Login frame)
+        {
+            this.frame = frame;
+        }
+
+        public bool isMittente()
+        {
+            bool temp = mittente;
+            mittente = false;
+            return temp;
+        }
+
+
+        public static Condivisa getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Condivisa();
+            }
+            return instance;
+        }
+    }
+}
---
+++ IndovinaChiCSharp/Gestore_pacchetti.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndovinaChiCSharp
+{
+    class Gestore_pacchetti
+    {
+        string connectedIP = null;
+        Condivisa c;
+        static Gestore_pacchetti instance = null;
+
+        private Gestore_pacchetti()
+        {
+            c = Condivisa.getInstance();
+        }
+
+        public static Gestore_pacchetti getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Gestore_pacchetti();
+            }
+            return instance;
+        }
+
+
+        public void execute(string s, string ip)
+        {
+            String action = s.Split(';')[0];
+            Console.WriteLine("ACTION: " + action);
+            if (action == ("a"))
+            {
+                connectedIP = ip;
+                Messaggio_Apertura ma = new Messaggio_Apertura();
+                ma.execute();
+            }
+            else if (ip == connectedIP)
+            {
+                switch (action)
+                {
+                    case "c":
+                        {
+                            Messaggio_Chiusura mc = new Messaggio_Chiusura();
+                            mc.execute();
+                            connectedIP = null;
+                            break;
+                        }
+                    case "m":
+                        {
+                            Messaggio_Testo mt = new Messaggio_Testo();
+                            mt.execute();
+                            break;
+                        }
+                    case "y":
+                        {
+                            connectedIP = ip;
+                            Messaggio_RispApertura mar = new Messaggio_RispApertura();
+                            mar.execute();
+                            break;
+                        }
+                    case "n":
+                        {
+                            connectedIP = null;
+                            break;
+                        }
+                }
+            }
+
+        }
+    }
+}
+
