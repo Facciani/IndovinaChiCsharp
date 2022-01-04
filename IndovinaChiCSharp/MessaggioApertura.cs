08a01d4a4da2587bd6031325440c5d2ee6973fa2
Facciani <alex03fa@gmail.com>
2022-01-04 16:43:20 +0100

    Correzione errore gestione pacchetto

---
+++ IndovinaChiCSharp/MessaggioApertura.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IndovinaChiCSharp
+{
+    class MessaggioApertura : Messaggio
+    {
+        String nomeMittente;
+
+        public MessaggioApertura(IPEndPoint riceveEP, byte[] dataReceived) : base(riceveEP,dataReceived)
+        {
+            nomeMittente = "";
+        }
+
+        public override void execute()
+        {
+            String[] csv = (Encoding.ASCII.GetString(packet)).Split(';');
+            DialogResult dialogResult = MessageBox.Show("Desideri accettare la connessione con: \n" + csv[1], "Avviso di connessione", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            String str = "";
+            if (dialogResult == DialogResult.Yes)
+            {
+               
+                c.connected = true;
+                str = ("y;" + c.nome + ";");
+                c.nomeDestinatario = csv[1];
+
+            }
+            else
+            {
+                str = "n;";
+            }
+            try
+            {
+                send(str);
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
