3ec1dcbc5c95f7d918dcd3e91986a46ade4eafb8
Facciani <alex03fa@gmail.com>
2022-01-04 15:21:38 +0100

    Modifica listen

--- IndovinaChiCSharp/Listen.cs
+++ IndovinaChiCSharp/Listen.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IndovinaChiCSharp
 {
-    class Listen
+    public class Listen
     {
         Condivisa c;
         Gestore_pacchetti gPacket;
@@ -25,9 +26,23 @@ namespace IndovinaChiCSharp
         {
             while(true)
             {
-                IPEndPoint riceveEP = new IPEndPoint(IPAddress.Any, 0);
-                byte[] dataReceived = client.Receive(ref riceveEP);
-                String risposta = Encoding.ASCII.GetString(dataReceived);
+                try
+                {
+                    IPEndPoint riceveEP = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] dataReceived = client.Receive(ref riceveEP);
+
+                    //String risposta = Encoding.ASCII.GetString(dataReceived);
+
+                    dataReceived = c.serverRicezione.Receive(ref riceveEP);
+                    gPacket.execute(riceveEP, dataReceived);
+
+                    Console.WriteLine("messaggio ricevuto");
+
+                }catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                Thread.Sleep(150);
             }
         }
 
